eeb0e19c2dd70c861e982b147d9f051d1699042b
larsvankleef <larsvankleef@outlook.com>
2015-04-10 14:33:00 +0200

    chage port

--- app.js
+++ app.js
@@ -32,8 +32,7 @@ io.on('connection', function(socket){
   	restloop();
   });
 });
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:80');
   console.log('Wating for user to connect');
 });
 
